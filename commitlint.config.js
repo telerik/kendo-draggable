f7df11509808d685e912d84802d4da384460769f
Tsvetomir Tsonev <tsvetomir.tsonev@progress.com>
2025-11-25 18:36:02 +0200

    chore: update dependencies

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert']],
+        'subject-max-length': [2, 'always', 200],
+    },
+};

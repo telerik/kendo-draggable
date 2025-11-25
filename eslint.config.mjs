41d2939a208b5baaa9d1fe48b5ed99264210be0f
Tsvetomir Tsonev <tsvetomir.tsonev@progress.com>
2025-11-25 18:46:13 +0200

    chore: upgrade eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from 'globals';
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+    {
+        files: ["**/*.js"],
+        plugins: {
+            js,
+        },
+        extends: ["js/recommended"],
+        languageOptions: {
+            globals: globals.browser
+        }
+    }
+]);
